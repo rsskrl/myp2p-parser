92e83f17279cb2e1eac5fd9906b7f4d0135c70a9
rsskrl <mm@c300.(none)>
2011-03-10 21:50:35 +0100

    increase sleep to 10 seconds because of slow servers

--- sopcast-play.sh
+++ sopcast-play.sh
@@ -12,7 +12,8 @@ IN_PORT="55558"
 killall -9 sp-sc-auth sp-sc 2>/dev/null
 
 sp-sc-auth $* ${IN_PORT} ${OUT_PORT} >/dev/null 2>/dev/null &
-sleep 3
+sleep 10 
 ${PLAYER} http://localhost:${OUT_PORT}/tv.asf
 
 killall -9 sp-sc-auth sp-sc 2>/dev/null
+
