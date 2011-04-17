7f9d6cbbb9722c94528d9d8ba426f45d03e991c2
rsskrl <mm@c300.(none)>
2011-04-17 16:50:04 +0200

    changed sp-sc-auth stopping command to a more selective pkill -f to stop only sopcast on these ports

--- sopcast-play.sh
+++ sopcast-play.sh
@@ -9,10 +9,15 @@ PLAYER="mplayer"
 OUT_PORT="55559"
 IN_PORT="55558"
 
-killall -9 sp-sc-auth sp-sc 2>/dev/null
+# killall existing sopcast streams on this port
+pkill -9 -f "sp-sc-auth .* ${IN_PORT} ${OUT_PORT}"
 
 sp-sc-auth $* ${IN_PORT} ${OUT_PORT} >/dev/null 2>/dev/null &
-sleep 10 
+
+# increase this to 15 or 20 if your connection
+# needs more time to establish connection
+sleep 10	
+
 ${PLAYER} http://localhost:${OUT_PORT}/tv.asf
 
 killall -9 sp-sc-auth sp-sc 2>/dev/null
